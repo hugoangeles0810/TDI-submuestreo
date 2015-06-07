0237ab19ee489dd42127d163a453d39de9dbc472
joeleche <joel_eche4@hotmail.com>
2015-06-07 17:01:08 -0500

    new file

---
+++ src/main/hola.java
@@ -0,0 +1,16 @@
+
+package main;
+
+public class Application {
+    
+    private Scanner input;
+
+    public Application() {
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");        
+    }
+}
